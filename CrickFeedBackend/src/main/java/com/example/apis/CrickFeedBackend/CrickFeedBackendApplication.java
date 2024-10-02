5c4f1c944aafff73aae6bd82a9d4a4522a66f2f6
Akshat Nigam <nigamakshat29jan2000@gmail.com>
2024-10-02 22:02:29 +0530

    initial project setup

---
+++ CrickFeedBackend/src/main/java/com/example/apis/CrickFeedBackend/CrickFeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.apis.CrickFeedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrickFeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrickFeedBackendApplication.class, args);
+	}
+
+}
---
+++ CrickFeedBackend/src/test/java/com/example/apis/CrickFeedBackend/CrickFeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apis.CrickFeedBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrickFeedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
